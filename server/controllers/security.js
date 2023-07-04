6d7cada95482792e18cd015c1ef7f91bdc9e4e73
a-bedda <ayman.bedda@l3m-holding.fr>
2023-07-04 22:15:34 +0200

    add Role and middleware check-role

--- server/controllers/security.js
+++ server/controllers/security.js
@@ -7,7 +7,7 @@ module.exports = function SecurityController(UserService) {
         const { email, password } = req.body;
         const user = await UserService.login(email, password);
         const token = jwt.sign(
-          { id: user.id, login: user.login },
+          { id: user.id, login: user.login, id_role: user.id_role },
           process.env.JWT_SECRET,
           {
             expiresIn: "1h",
