00267e575e56e9beff996c9d7fe70bccc3d89b77
simonBtssio <simon.farnault.sio@gmail.com>
2023-07-25 17:38:03 +0200

    ouai

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,12 +1,24 @@
     <script setup>
-        import { onMounted } from 'vue';
         import { useRoute, useRouter } from 'vue-router';
         import BoardContainer from "../components/BoardContainer.vue";
+        import { onMounted, ref } from 'vue';
 
-        const route = useRoute();
+        const gameId = ref(useRoute().params.gameId);
         const router = useRouter();
-        let gameId = route.params.gameId;
 
+        onMounted(async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/games/${gameId.value}`, {method: 'GET'});
+            const gameExists = await response.json();
+
+            if (!gameExists) {
+            router.replace({ name: 'NotFound' });
+            }
+        } catch (error) {
+            console.error(error);
+            router.replace({ name: 'NotFound' });
+        }
+        });
     </script>
 
     <template>
--- server/routes/game.js
+++ server/routes/game.js
@@ -8,6 +8,12 @@ const router = new Router();
 const Controller = new GameController(new GameService());
 
 router.use("/", new genericRouter(new genericController(new GameService())));
+<<<<<<< HEAD
 router.get("/:id/authorized", Controller.authorized);
+=======
+router.post("/new", Controller.new);
+router.get("/:session", Controller.exist);
+router.get("/test", Controller.test);
+>>>>>>> muthu ouai ouai
 
 module.exports = router;
