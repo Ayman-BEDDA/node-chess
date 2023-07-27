17ddae9d634cb2a321581f45acd8a1ed3c13f3e4
simonBtssio <simon.farnault.sio@gmail.com>
2023-07-27 11:16:36 +0200

    3

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    port: 8080, // Ajoutez cette ligne
+    host: '0.0.0.0' // Décommentez cette ligne si vous voulez que votre application soit accessible à partir d'autres machines sur le réseau
+  },
 })
